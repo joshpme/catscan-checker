332163eedf60527238b376c21cfe3f002b2be5ae
Josh P <github@joshp.me>
2024-11-25 15:59:06 +1100

    Switch back to python 3.11

--- packages/catscan/notify/build.sh
+++ packages/catscan/notify/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 virtualenv --without-pip virtualenv
-pip install -r requirements.txt --target virtualenv/lib/python3.12/site-packages
+pip install -r requirements.txt --target virtualenv/lib/python3.11/site-packages
