61683775459c7753b333233126c3e2d017faae01
Josh P <github@joshp.me>
2025-05-04 17:29:44 +1000

    Add example line

--- packages/catscan/finder/indico.py
+++ packages/catscan/finder/indico.py
@@ -83,6 +83,7 @@ def find_contributions(event_id, exclude_list=None):
     contribution_revision_tuples = []  # (contribution_id, revision_id)
 
     #def find_papers(event_id):
+    requests.get("https://indico.jacow.org/event/37/editing/api/paper/list")
     # requests.get(os.getenv("INDICO_BASE_URL") + f"/event/{event_id}/editing/api/paper/list", {
     #     "Authorization": f"Bearer {os.getenv("INDICO_TOKEN")}"
     # })
