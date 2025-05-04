e1ec792b63f014161fbe508fb8f31c0c92e6a66e
Josh P <github@joshp.me>
2025-05-04 13:01:04 +1000

    Add body return to finder call

--- packages/catscan/finder/__main__.py
+++ packages/catscan/finder/__main__.py
@@ -11,11 +11,13 @@ sentry_sdk.init(
 def main(event):
     try:
         event_id = event.get("id", None)
+        if event_id is None:
+            return {"body": {"error": "Event ID is required"}}
         start_timer = time.time()
         queue_items, cached_items, errors = runner(event_id=event_id)
         end_timer = time.time()
         sentry_sdk.capture_event({
-            "message": "Runner ran",
+            "message": "Finder ran",
             "level": "info",
             "extra": {"event_id": event_id,
                       "duration": end_timer - start_timer,
@@ -23,7 +25,10 @@ def main(event):
                       "cached_items": cached_items,
                       "errors": errors},
         })
+        return {"body": {"queue_items": queue_items,
+                    "cached_items": cached_items,
+                    "errors": errors}}
     except BaseException as e:
-        error_msg = f"Worker: An unexpected error occurred.\n Details:\n {e=}"
+        error_msg = f"Finder: An unexpected error occurred.\n Details:\n {e=}"
         sentry_sdk.capture_message(error_msg, level="error")
         return {"body": {"error": error_msg}}
