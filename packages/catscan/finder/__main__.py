0e57def2084092522a3a8e7cee2e4e9192d41d06
Josh P <github@joshp.me>
2025-05-04 14:01:54 +1000

    Leave import present and return example data

--- packages/catscan/finder/__main__.py
+++ packages/catscan/finder/__main__.py
@@ -15,7 +15,7 @@ def main(event):
         event_id = event.get("id", None)
         if event_id is None:
             return {"body": {"error": "Event ID is required"}}
-        queue_items, cached_items, errors = finder(event_id=event_id)
+        queue_items, cached_items, errors = [],[],None #finder(event_id=event_id)
         end_timer = time.time()
         output = {
             "event_id": event_id,
