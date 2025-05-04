d3a555f177a7ca066fdf1536aabb0b6a5588c76c
Josh P <github@joshp.me>
2025-05-04 13:32:29 +1000

    Determine where issue is occurring

--- packages/catscan/finder/__main__.py
+++ packages/catscan/finder/__main__.py
@@ -3,32 +3,42 @@ import time
 import sentry_sdk
 from runner import runner
 
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    send_default_pii=True,
-)
+# sentry_sdk.init(
+#     dsn=os.getenv("SENTRY_DSN"),
+#     send_default_pii=True,
+# )
 
 def main(event):
     try:
         event_id = event.get("id", None)
         if event_id is None:
             return {"body": {"error": "Event ID is required"}}
-        start_timer = time.time()
-        queue_items, cached_items, errors = runner(event_id=event_id)
-        end_timer = time.time()
-        sentry_sdk.capture_event({
-            "message": "Finder ran",
-            "level": "info",
-            "extra": {"event_id": event_id,
-                      "duration": end_timer - start_timer,
-                      "queue_items": queue_items,
-                      "cached_items": cached_items,
-                      "errors": errors},
-        })
-        return {"body": {"queue_items": queue_items,
-                    "cached_items": cached_items,
-                    "errors": errors}}
+
+        return {"body": {"error": "This function is not meant to be called directly."}}
     except BaseException as e:
         error_msg = f"Finder: An unexpected error occurred.\n Details:\n {e=}"
-        sentry_sdk.capture_message(error_msg, level="error")
         return {"body": {"error": error_msg}}
+
+    # try:
+    #     event_id = event.get("id", None)
+    #     if event_id is None:
+    #         return {"body": {"error": "Event ID is required"}}
+    #     start_timer = time.time()
+    #     queue_items, cached_items, errors = runner(event_id=event_id)
+    #     end_timer = time.time()
+    #     sentry_sdk.capture_event({
+    #         "message": "Finder ran",
+    #         "level": "info",
+    #         "extra": {"event_id": event_id,
+    #                   "duration": end_timer - start_timer,
+    #                   "queue_items": queue_items,
+    #                   "cached_items": cached_items,
+    #                   "errors": errors},
+    #     })
+    #     return {"body": {"queue_items": queue_items,
+    #                 "cached_items": cached_items,
+    #                 "errors": errors}}
+    # except BaseException as e:
+    #     error_msg = f"Finder: An unexpected error occurred.\n Details:\n {e=}"
+    #     sentry_sdk.capture_message(error_msg, level="error")
+    #     return {"body": {"error": error_msg}}
