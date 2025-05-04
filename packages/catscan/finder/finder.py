8583e42183fbf7536f5cd8b48d9dca0d057b5fe8
Josh P <github@joshp.me>
2025-05-04 14:42:23 +1000

    Remove all imports from finder

--- packages/catscan/finder/finder.py
+++ packages/catscan/finder/finder.py
@@ -1,5 +1,5 @@
 # from db import connect_db, get_cache, append_cache, append_queue, find_recent_queue_items
-from indico import find_all_contributions_with_no_catscan_comment
+#from indico import find_all_contributions_with_no_catscan_comment
 
 def finder(event_id):
     error_list = []
@@ -10,10 +10,10 @@ def finder(event_id):
     #     cache_list = get_cache(cnx, event_id)
     #     recently_queued = find_recent_queue_items(cnx, event_id)
     #     exclude_list = recently_queued + cache_list
-    to_check, new_exclude_list, find_error = find_all_contributions_with_no_catscan_comment(event_id,
-                                                                                            exclude_list=exclude_list)
-    if find_error is not None:
-        error_list.append(f"Error finding contributions: {find_error}")
+    # to_check, new_exclude_list, find_error = find_all_contributions_with_no_catscan_comment(event_id,
+    #                                                                                         exclude_list=exclude_list)
+    # if find_error is not None:
+    #     error_list.append(f"Error finding contributions: {find_error}")
     #      for contrib_id in new_exclude_list:
     #          cache_error = append_cache(cnx, event_id, contrib_id, "unknown")
     #          if cache_error is not None:
@@ -26,3 +26,4 @@ def finder(event_id):
     #       queue_items = len(to_check)
     #       cached_items = len(new_exclude_list)
     return queue_items, cached_items, error_list
+
